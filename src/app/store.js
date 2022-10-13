53566e9060f56d0d25eee376c545ad6abee704c6
sayeef-rahman <sayeef.cse@gmial.com>
2022-10-13 11:23:49 +0600

    default middleware added

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 import counterSlice from "../features/counter/counterSlice";
 
 const store = configureStore({
     reducer:{
         counter: counterSlice
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 });
 
 export default store;
